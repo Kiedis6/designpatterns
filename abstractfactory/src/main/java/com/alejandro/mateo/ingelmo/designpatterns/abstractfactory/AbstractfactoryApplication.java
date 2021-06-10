c5aec0628d5373ba3b0119f52ca8b5e78a551372
Alejandro Mateo <kiedis286@gmail.com>
2021-06-10 10:16:57 +0200

    Initial commit

---
+++ abstractfactory/src/main/java/com/alejandro/mateo/ingelmo/designpatterns/abstractfactory/AbstractfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.mateo.ingelmo.designpatterns.abstractfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractfactoryApplication.class, args);
+	}
+
+}
